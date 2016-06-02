7a6c412406cd31b71dff2d890765c308231f0135
Luke Gessler <ldg3fa@virginia.edu>
2016-06-02 19:29:21 -0400

    py3

--- 01_ants_js/serve.py
+++ 01_ants_js/serve.py
@@ -8,5 +8,5 @@ Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "serving at port", PORT
+print("serving at port", PORT)
 httpd.serve_forever()
