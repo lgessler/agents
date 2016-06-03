329a8ccebfbaaaacd2235654dc3cd87c75eaea74
Luke Gessler <ldg3fa@virginia.edu>
2016-06-02 21:08:04 -0400

    config

---
+++ 01_ants_js/config.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+
+GAME_WIDTH = 800;
+GAME_HEIGHT = 640;
+
+FACTION = {
+  NUM: 3,
+  COLORS: {
+    0: [255, 0, 0],
+    1: [0, 255, 0],
+    2: [0, 0, 255]
+  }
+};
+
+};
