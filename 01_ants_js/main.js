f082e3f0bb41d8cf41f58465efebb30af9b523ee
Luke Gessler <ldg3fa@virginia.edu>
2016-06-02 23:29:28 -0400

    finish stubs

--- 01_ants_js/main.js
+++ 01_ants_js/main.js
@@ -1,46 +1,86 @@
 window.onload = function() {
 
-var map, layer, cursors;
+// Globals
+var antList;
+var foodList;
 
+/********************
+ * Phaser functions *
+ ********************/
 function preload() {
-  game.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
-};
+  game.load.image('ant', 'assets/brick.png');
+}
 
 function create() {
-  map = game.add.tilemap('map', 16, 16);
-  layer = map.createLayer(0);
-  layer.resizeWorld();
-  cursors = game.input.keyboard.createCursorKeys();
-
-  var help = game.add.text(16, 16, 'Arrows to scroll', 
-      { font: '14px Arial', fill: '#ffffff' });
-  help.fixedToCamera = true;
-};
+  foodList = [];
+  antList = [];
+  spawnInitAnts(INIT_NUM_OF_ANTS);
+  //spawnInitFood()
+  //spawnInitDirt()
+}
 
 function update() {
-  if (cursors.left.isDown) {
-    game.camera.x -= 4;
-  } 
-  else if (cursors.right.isDown) {
-    game.camera.x += 4;
-  }
-  if (cursors.up.isDown) {
-    game.camera.y -= 4;
-  } 
-  else if (cursors.down.isDown) {
-    game.camera.y += 4;
-  }
-};
+  spawnAnts();
+  //spawnFood();
+  //spawnDirt();
+
+  antList.forEach(function(ant) {
+    //ant.checkSurroundings();
+    //ant.decide();
+    //ant.act();
+  });
+}
 
 function render() {
 
-};
+}
 
-var game = new Phaser.Game(800, 640, Phaser.AUTO, 'ants',
+var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'ants',
     { preload: preload, create: create, update: update, render: render });
 
+/************************
+ * Our helper functions *
+ ************************/
+
+function randRange(r) { return Math.floor(Math.random() * r); }
+var randX = function() { return randRange(GAME_WIDTH) };
+var randY = function() { return randRange(GAME_HEIGHT) };
+
+function spawnInitAnts(x) {
+  for (var i = 0; i < FACTION.NUM; i++) {
+    var loc = [randX(), randY()];
+    for (var j = 0; j < x/3; j++) { 
+      spawnAnt(i, loc);
+    }
+  }
+}
+
+function spawnAnt(faction, loc) {
+  var sprite = game.add.sprite(loc[0], loc[1], 'ant');
+  var rgb = FACTION.COLORS[faction];
+  sprite.tint = rgb[0] << 16 | rgb[1] << 8 | rgb[2];
+  antList.push(new Ant(undefined, loc, undefined, 
+        undefined, undefined, undefined, faction, undefined, sprite));
+}
+
+function spawnAnts() {
+  if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+    var faction = randint(0, FACTION.NUM);
+    spawnAnt(faction, [game.input.x, game.input.y]);
+  }
+  for (var i = 0; i < 10; i++) {
+    var key = dig2eng[(i + 1) % 10];
+    if (eval("game.input.keyboard.isDown(Phaser.Keyboard." + key + ")")) {
+      spawnAnt(i, [game.input.x, game.input.y]);
+    }
+  }
+}
 
 
 };
 
-
+/*
+  var help = game.add.text(16, 16, 'Arrows to scroll', 
+      { font: '14px Arial', fill: '#ffffff' });
+  help.fixedToCamera = true;
+*/
