2efef6eb9739f90eccd23b76ab26a78405fee6c0
Luke Gessler <ldg3fa@virginia.edu>
2016-06-02 20:16:55 -0400

    stub out ant

---
+++ 01_ants_js/ant.js
@@ -0,0 +1,16 @@
+// constructor
+var Ant = function() {
+  // this.x = y;
+};
+
+Ant.prototype = {
+  x: 1,
+  y: 2,
+  name: "hello"
+}
+
+// OR
+
+Ant.prototype.x = 1;
+Ant.prototype.y = 2;
+Ant.prototype.name = "hello";
